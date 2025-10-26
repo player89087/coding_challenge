935b2ac04af65f600d301c13e2d40be5c18c3773
player89087 <edar.horst@gmail.com>
2025-10-26 20:01:21 +0100

    changed challenge, did solution

--- solution_0.py
+++ solution_0.py
@@ -1,13 +1,14 @@
-from decimal import * 
-numb = int(input("Amount: "))
+import sys 
+
+sys.set_int_max_str_digits(100000)
+numb = 200000
 prev = 1
 prev2 = 0
 new = 0 
-for i in range(0,numb):
-    new = prev + prev2
-    if i == numb -1 : 
-         golden_ratio = Decimal(new) / Decimal(prev) # relationship between two sections
-    prev2 = prev
-    prev = new
-    
-
+for i in range(0,200000):
+     new = prev + prev2
+     prev2 = prev
+     prev = new
+     
+new = str(new)        
+print(len(new))
