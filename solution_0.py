bf8b786dc3a887f62d1f99f05dd720099b1ad00c
player89087 <edar.horst@gmail.com>
2025-10-26 21:32:34 +0100

    fixed error, iwas to low, now 200002,

--- solution_0.py
+++ solution_0.py
@@ -1,14 +1,14 @@
 import sys 
 
-sys.set_int_max_str_digits(100000)
-numb = 200000
+sys.set_int_max_str_digits(1000000)
 prev = 1
 prev2 = 0
 new = 0 
-for i in range(0,200000):
+for i in range(0,200002):
      new = prev + prev2
      prev2 = prev
      prev = new
-     
+
+
 new = str(new)        
 print(len(new))
