cb9852847d90219561fb18344abb4b80d976fbe5
player89087 <edar.horst@gmail.com>
2026-02-04 10:47:59 +0100

    works fine, may change the text, runtime can be improved

--- solutions/level3/creation/create_img.py
+++ solutions/level3/creation/create_img.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-file = r"/home/siredgar20/Schreibtisch/coding_challenge/solutions/level3/challenge_img.png"
+file = r"/home/siredgar20/Schreibtisch/coding_challenge/solutions/level3/original_img.png"
 with Image.open(file) as img:
     x,y = img.size
     img = img.convert("RGB")
@@ -9,18 +9,20 @@ cord_y = []
 for i in range(0,x):
     for g in range(0,y):
         if img.getpixel((i,g)) != (255,255,255): 
+            continue 
+        elif img.getpixel((i,g)) <= (255,255,255) and img.getpixel((i,g)) >= (240,240,240):
             cord_x.append(i)
             cord_y.append(g)
             
+       # elif img.getpixel(i,g) ==
 print(cord_x)
 print(cord_y)
 
-img_new = Image.new("RGB",(x,y),(0,0,0))
+img_new = Image.new("RGB",(x,y),(255,255,255))
 
 px = img_new.load() # always with () otherwise error
 
 for x,y in  zip(cord_x,cord_y):
-    px[x,y] = (255,255,255)  
+    px[x,y] = (254,254,254)  
 
 img_new.show()
-
